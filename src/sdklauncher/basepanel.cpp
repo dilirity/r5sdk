dilirity/r5sdk

commit 55f459b65271fd6426cefbd12f57a8f7ab840f81

VpkLib: replace STD regex with Boost.Regex
boost::regex is a lot faster than std::regex, switch to boost for more performance.

src/sdklauncher/basepanel.cpp
@@ -760,37 +760,41 @@ void CSurface::ParseMaps()
 	}
 
 	fs::directory_iterator directoryIterator(vpkPath);
-	std::cmatch regexMatches;
+	boost::cmatch regexMatches;
 
 	for (const fs::directory_entry& directoryEntry : directoryIterator)
 	{
-		std::string fileName = directoryEntry.path().u8string();
-		std::regex_search(fileName.c_str(), regexMatches, g_VpkDirFileRegex);
+		const std::string fileName = directoryEntry.path().u8string();
+		const char* const pFileName = fileName.c_str();
 
-		if (!regexMatches.empty())
+		const bool result = boost::regex_search(pFileName, &pFileName[fileName.length()], regexMatches, g_VpkDirFileRegex);
+
+		if (!result || !regexMatches.empty())
 		{
-			const std::sub_match<const char*>& match = regexMatches[2];
+			continue;
+		}
 
-			if (match.compare("frontend") == 0)
-			{
-				continue;
-			}
-			else if (match.compare("mp_common") == 0)
-			{
-				if (!this->m_MapCombo->Items.Contains("mp_lobby"))
-				{
-					this->m_MapCombo->Items.Add("mp_lobby");
-				}
-				continue;
-			}
-			else
-			{
-				const string mapName = match.str();
+		const boost::sub_match<const char*>& match = regexMatches[2];
 
-				if (!this->m_MapCombo->Items.Contains(match.str().c_str()))
-				{
-					this->m_MapCombo->Items.Add(match.str().c_str());
-				}
+		if (match.compare("frontend") == 0)
+		{
+			continue;
+		}
+		else if (match.compare("mp_common") == 0)
+		{
+			if (!this->m_MapCombo->Items.Contains("mp_lobby"))
+			{
+				this->m_MapCombo->Items.Add("mp_lobby");
+			}
+			continue;
+		}
+		else
+		{
+			const string mapName = match.str();
+
+			if (!this->m_MapCombo->Items.Contains(match.str().c_str()))
+			{
+				this->m_MapCombo->Items.Add(match.str().c_str());
 			}
 		}
 	}

src/engine/cmodel_bsp.cpp
@@ -165,7 +165,7 @@ void Mod_GetAllInstalledMaps()
     CUtlVector<CUtlString> fileList;
     AddFilesToList(fileList, "vpk", "vpk", nullptr, '/');
 
-    std::cmatch regexMatches;
+    boost::cmatch regexMatches;
     AUTO_LOCK(g_InstalledMapsMutex);
 
     g_InstalledMaps.Purge(); // Clear current list.
@@ -173,18 +173,30 @@ void Mod_GetAllInstalledMaps()
     FOR_EACH_VEC(fileList, i)
     {
         const CUtlString& filePath = fileList[i];
-        const char* pFileName = strrchr(filePath.Get(), '/')+1;
 
-        // Should always point right in front of the last
-        // slash, as the files are loaded from 'vpk/'.
-        Assert(pFileName);
+        const char* const pathBase = filePath.String();
+        const ssize_t pathLength = filePath.Length();
 
-        const bool result = std::regex_search(pFileName, regexMatches, g_VpkDirFileRegex);
+        ssize_t fileNameStart = pathLength;
+
+        // Get the unqualified file name.
+        while (fileNameStart)
+        {
+            if (pathBase[fileNameStart] == '/')
+            {
+                fileNameStart++; // Skip the '/'.
+                break;
+            }
+
+            fileNameStart--;
+        }
+
+        const bool result = boost::regex_match(&pathBase[fileNameStart], &pathBase[pathLength], regexMatches, g_VpkDirFileRegex);
 
         if (!result || regexMatches.empty())
             continue;
 
-        const std::sub_match<const char*>& match = regexMatches[2];
+        const boost::csub_match& match = regexMatches[2];
 
         if (match.compare("frontend") == 0)
             continue; // Frontend contains no BSP's.

src/vpklib/packedstore.cpp
@@ -95,8 +95,8 @@ bool PackedStore_GetDirBaseName(const CUtlString& dirFileName, CUtlString& dirBa
 {
 	const char* baseFileName = V_UnqualifiedFileName(dirFileName.String());
 
-	std::cmatch regexMatches;
-	const bool result = std::regex_search(baseFileName, regexMatches, g_VpkDirFileRegex);
+	boost::cmatch regexMatches;
+	const bool result = boost::regex_search(baseFileName, regexMatches, g_VpkDirFileRegex);
 
 	if (!result || regexMatches.size() < 3)
 		return false;
@@ -112,12 +112,12 @@ bool PackedStore_GetDirBaseName(const CUtlString& dirFileName, CUtlString& dirBa
 //          &dirBaseName  - <- part of directory file name as string
 // Output : true on success, false otherwise
 //-----------------------------------------------------------------------------
-bool PackedStore_GetDirNameParts(const CUtlString& dirFileName, const size_t nCaptureGroup, CUtlString& dirNameParts)
+bool PackedStore_GetDirNameParts(const CUtlString& dirFileName, const int nCaptureGroup, CUtlString& dirNameParts)
 {
 	const char* baseFileName = V_UnqualifiedFileName(dirFileName.String());
 
-	std::cmatch regexMatches;
-	const bool result = std::regex_search(baseFileName, regexMatches, g_VpkDirFileRegex);
+	boost::cmatch regexMatches;
+	const bool result = boost::regex_search(baseFileName, regexMatches, g_VpkDirFileRegex);
 
 	if (!result || regexMatches.size() < (nCaptureGroup + 1))
 		return false;
@@ -867,8 +867,8 @@ VPKDir_t::VPKDir_t(const CUtlString& dirFilePath, bool bSanitizeName)
 		return;
 	}
 
-	std::cmatch regexMatches;
-	const bool result = std::regex_search(dirFilePath.String(), regexMatches, g_VpkPackFileRegex);
+	boost::cmatch regexMatches;
+	const bool result = boost::regex_search(dirFilePath.String(), regexMatches, g_VpkPackFileRegex);
 
 	if (!result || regexMatches.empty()) // Not a block file, or not following the naming scheme.
 	{

src/vpklib/packedstore.h
@@ -27,8 +27,8 @@ constexpr int PACKFILEINDEX_SEP = 0x0;
 constexpr int PACKFILEINDEX_END = 0xffff;
 constexpr const char VPK_IGNORE_FILE[] = ".vpkignore";
 
-static const std::regex g_VpkDirFileRegex{ R"((?:.*\/)?([^_]*)(?:_)(.*)(.bsp.pak000_dir).*)" };
-static const std::regex g_VpkPackFileRegex{ R"(pak000_([0-9]{3}))" };
+static const boost::regex g_VpkDirFileRegex{ R"((?:.*\/)?([^_]*)(?:_)(.*)(.bsp.pak000_dir).*)" };
+static const boost::regex g_VpkPackFileRegex{ R"(pak000_([0-9]{3}))" };
 
 //-----------------------------------------------------------------------------
 // KeyValues structure for the VPK manifest file. This struct gets populated by
@@ -221,7 +221,7 @@ private:
 };
 
 bool PackedStore_GetDirBaseName(const CUtlString& dirFileName, CUtlString& dirBaseName);
-bool PackedStore_GetDirNameParts(const CUtlString& dirFileName, const size_t nCaptureGroup, CUtlString& dirNameParts);
+bool PackedStore_GetDirNameParts(const CUtlString& dirFileName, const int nCaptureGroup, CUtlString& dirNameParts);
 ///////////////////////////////////////////////////////////////////////////////
 
 #endif // PACKEDSTORE_H

commit 389248581cb033d35382cf94dd2ad1e413db9a58

Launcher: fix map list regression
Regression introduced in commit 55f459b65271fd6426cefbd12f57a8f7ab840f81 after trying to reduce the indentation level of the code, but the condition was never flipped.

src/sdklauncher/basepanel.cpp
@@ -769,7 +769,7 @@ void CSurface::ParseMaps()
 
 		const bool result = boost::regex_search(pFileName, &pFileName[fileName.length()], regexMatches, g_VpkDirFileRegex);
 
-		if (!result || !regexMatches.empty())
+		if (!result || regexMatches.empty())
 		{
 			continue;
 		}