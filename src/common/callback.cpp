dilirity/r5sdk

commit 6b17773f130f81140ef676f57f174b9d835349be

Common: fix "changelevel" command handler when loaded in the lobby
The "map" command didn't work in mp_lobby, because the "map" command calls the original "changelevel" command handler internally if we are loaded in a map. If we are in mp_lobby, the code enforces additional checks on the local Origin and Nucleus token. These tokens are not required for local multiplayer play, only if you play on secure multiplayer server, and therefore this check is redundant. Hook it against our own "changelevel" command handler so that all internal calls get routed through ours instead. This patch also fixes the CreateServer() script function not working when being loaded in the lobby.

src/common/callback.cpp
@@ -607,6 +607,7 @@ void UIScript_Reset_f()
 
 void VCallback::Detour(const bool bAttach) const
 {
+	DetourSetup(&v__Host_ChangeLevel_f, &Host_Changelevel_f, bAttach);
 	DetourSetup(&v__Cmd_Exec_f, &Cmd_Exec_f, bAttach);
 #ifndef DEDICATED
 	DetourSetup(&v__UIScript_Reset_f, &UIScript_Reset_f, bAttach);

src/common/callback.h
@@ -3,6 +3,7 @@
 inline bool(*v_SetupGamemode)(const char* pszPlayList);
 
 /* ==== CONCOMMANDCALLBACK ============================================================================================================================================== */
+inline void(*v__Host_ChangeLevel_f)(const CCommand& args);
 inline void(*v__Cmd_Exec_f)(const CCommand& args);
 inline void(*v__setClassVarServer_f)(const CCommand& args);
 inline void(*v__setClassVarClient_f)(const CCommand& args);
@@ -48,6 +49,7 @@ class VCallback : public IDetour
 	virtual void GetAdr(void) const
 	{
 		LogFunAdr("SetupGamemode", v_SetupGamemode);
+		LogFunAdr("Host_ChangeLevel_f", v__Host_ChangeLevel_f);
 		LogFunAdr("Cmd_Exec_f", v__Cmd_Exec_f);
 		LogFunAdr("SetClassVarServer_f", v__setClassVarServer_f);
 		LogFunAdr("SetClassVarClient_f", v__setClassVarClient_f);
@@ -58,6 +60,7 @@ class VCallback : public IDetour
 	virtual void GetFun(void) const
 	{
 		Module_FindPattern(g_GameDll, "40 53 48 83 EC 20 48 8B D9 48 C7 C0 ?? ?? ?? ??").GetPtr(v_SetupGamemode);
+		Module_FindPattern(g_GameDll, "40 53 48 83 EC ?? 83 79 ?? ?? 48 8B D9 7C ?? 83 3D").GetPtr(v__Host_ChangeLevel_f);
 		Module_FindPattern(g_GameDll, "40 55 53 48 8D AC 24 ?? ?? ?? ?? B8 ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 2B E0 48 8B D9").GetPtr(v__Cmd_Exec_f);
 		Module_FindPattern(g_GameDll, "41 56 48 83 EC ?? 8B 05 ?? ?? ?? ?? 4C 8B F1 FF C0").GetPtr(v__setClassVarServer_f);
 		Module_FindPattern(g_GameDll, "4C 8B DC 57 48 81 EC ?? ?? ?? ?? 8B 05").GetPtr(v__setClassVarClient_f);

commit 8088ddfde35a52314c85424105940f743897a63a

Common: fix client.dll compile error
Not available for the client library, host only. Compile error was introduced in commit 6b17773f130f81140ef676f57f174b9d835349be.

src/common/callback.cpp
@@ -607,7 +607,9 @@ void UIScript_Reset_f()
 
 void VCallback::Detour(const bool bAttach) const
 {
+#ifndef CLIENT_DLL
 	DetourSetup(&v__Host_ChangeLevel_f, &Host_Changelevel_f, bAttach);
+#endif // !CLIENT_DLL
 	DetourSetup(&v__Cmd_Exec_f, &Cmd_Exec_f, bAttach);
 #ifndef DEDICATED
 	DetourSetup(&v__UIScript_Reset_f, &UIScript_Reset_f, bAttach);

src/common/callback.h
@@ -3,7 +3,9 @@
 inline bool(*v_SetupGamemode)(const char* pszPlayList);
 
 /* ==== CONCOMMANDCALLBACK ============================================================================================================================================== */
+#ifndef CLIENT_DLL
 inline void(*v__Host_ChangeLevel_f)(const CCommand& args);
+#endif // !CLIENT_DLL
 inline void(*v__Cmd_Exec_f)(const CCommand& args);
 inline void(*v__setClassVarServer_f)(const CCommand& args);
 inline void(*v__setClassVarClient_f)(const CCommand& args);
@@ -49,7 +51,9 @@ class VCallback : public IDetour
 	virtual void GetAdr(void) const
 	{
 		LogFunAdr("SetupGamemode", v_SetupGamemode);
+#ifndef CLIENT_DLL
 		LogFunAdr("Host_ChangeLevel_f", v__Host_ChangeLevel_f);
+#endif // !CLIENT_DLL
 		LogFunAdr("Cmd_Exec_f", v__Cmd_Exec_f);
 		LogFunAdr("SetClassVarServer_f", v__setClassVarServer_f);
 		LogFunAdr("SetClassVarClient_f", v__setClassVarClient_f);
@@ -60,7 +64,9 @@ class VCallback : public IDetour
 	virtual void GetFun(void) const
 	{
 		Module_FindPattern(g_GameDll, "40 53 48 83 EC 20 48 8B D9 48 C7 C0 ?? ?? ?? ??").GetPtr(v_SetupGamemode);
+#ifndef CLIENT_DLL
 		Module_FindPattern(g_GameDll, "40 53 48 83 EC ?? 83 79 ?? ?? 48 8B D9 7C ?? 83 3D").GetPtr(v__Host_ChangeLevel_f);
+#endif // !CLIENT_DLL
 		Module_FindPattern(g_GameDll, "40 55 53 48 8D AC 24 ?? ?? ?? ?? B8 ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 2B E0 48 8B D9").GetPtr(v__Cmd_Exec_f);
 		Module_FindPattern(g_GameDll, "41 56 48 83 EC ?? 8B 05 ?? ?? ?? ?? 4C 8B F1 FF C0").GetPtr(v__setClassVarServer_f);
 		Module_FindPattern(g_GameDll, "4C 8B DC 57 48 81 EC ?? ?? ?? ?? 8B 05").GetPtr(v__setClassVarClient_f);