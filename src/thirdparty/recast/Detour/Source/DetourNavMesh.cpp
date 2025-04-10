dilirity/r5sdk

commit dffe3d5510c05cb35f215072788668d56261cd14

Recast: fix crash during prune of post-unlinked off-mesh connection
If you build a NavMesh and remove a tile, and that tile happens to be the land tile of one or more off-mesh connection, it's polygon will still have the `DT_POLYFLAGS_JUMP_LINKED` flag causing a null pointer dereference in `dtUpdateNavMeshData()`. This function drops every off-mesh connection that doesn't have the `DT_POLYFLAGS_JUMP_LINKED`, but it fixes the polygon references within the land tile if it does have this flag. But since we removed this tile, `dtNavMesh::getTileAt()` will return NULL for the given tile location.
The fix is to update the polygon flags in `dtNavMesh::unconnectLinks()` when its an off-mesh connection.

src/thirdparty/recast/Detour/Source/DetourNavMesh.cpp
@@ -511,6 +511,9 @@ void dtNavMesh::unconnectLinks(dtMeshTile* tile, dtMeshTile* target)
 				j = tile->links[j].next;
 			}
 		}
+
+		if (poly->getType() == DT_POLYTYPE_OFFMESH_CONNECTION)
+			poly->flags &= ~DT_POLYFLAGS_JUMP_LINKED;
 	}
 }
 

src/thirdparty/recast/Detour/Source/DetourNavMeshBuilder.cpp
@@ -1726,6 +1726,12 @@ bool dtUpdateNavMeshData(dtNavMesh* nav, const unsigned int tileIndex)
 
 		const dtMeshTile* landTile = nav->getTileAt(tx, ty, header->layer);
 
+		// note(amos): this gets triggered when the land tile of the off-mesh
+		// connection was removed but the off-mesh connection poly flags still
+		// had the DT_POLYFLAGS_JUMP_LINKED flag. This means there is a code
+		// bug somewhere. (See dtNavMesh::unconnectLinks for the solution.)
+		rdAssert(landTile);
+
 		if (landTile == tile)
 			continue; // Already dealt with when fixing up internal links.
 

commit 492923e0d753bd82cab52b905eea7b67ddf1b18f

Recast: fix incorrect placement of unlinked off-mesh connection tagging
Regression was caused from a mistake in commit dffe3d5510c05cb35f215072788668d56261cd14 when trying to fix an issue where these never got tagged. The flag must only be removed if the actual poly is getting unlinked, not just traversed.

src/thirdparty/recast/Detour/Source/DetourNavMesh.cpp
@@ -501,6 +501,13 @@ void dtNavMesh::unconnectLinks(dtMeshTile* tile, dtMeshTile* target)
 					poly->firstLink = nj;
 				else
 					tile->links[pj].next = nj;
+
+				// note(kawe): If we unlink the target from the off-mesh
+				// connection, we must mark this off-mesh connection as
+				// open so that it can be removed in dtUpdateNavMeshData.
+				if (poly->getType() == DT_POLYTYPE_OFFMESH_CONNECTION)
+					poly->flags &= ~DT_POLYFLAGS_JUMP_LINKED;
+
 				tile->freeLink(j);
 				j = nj;
 			}
@@ -511,9 +518,6 @@ void dtNavMesh::unconnectLinks(dtMeshTile* tile, dtMeshTile* target)
 				j = tile->links[j].next;
 			}
 		}
-
-		if (poly->getType() == DT_POLYTYPE_OFFMESH_CONNECTION)
-			poly->flags &= ~DT_POLYFLAGS_JUMP_LINKED;
 	}
 }
 