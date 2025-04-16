dilirity/r5sdk

commit 17b37ff7d2e49810c532fdf24f493372b37cc32d

Miles: rewrite CSOM async file callbacks; partially finished sound engine upgrade
Full rewrite of the CSOM async file callbacks, this is required for the upgrade to Miles Sound System 10.0.62.0 as the MilesAsyncRead structure has undergone a refactor. The new Miles Sound System upgrade works.

Current regressions:
- Closed captions (sub-titles) do not disappear with the audio event's time correctly; usages of `MilesEventInfoQueueEnum` need to be looked at.

src/codecs/miles/miles_impl.cpp
@@ -2,9 +2,13 @@
 #include "tier0/fasttimer.h"
 #include "tier0/commandline.h"
 #include "tier1/cvar.h"
+#include "rtech/core/strutils.h"
+#include "rtech/async/asyncio.h"
+#include "rtech/pak/pakstate.h"
 #include "filesystem/filesystem.h"
 #include "ebisusdk/EbisuSDK.h"
 #include "miles_impl.h"
+#include "miles/src/sdk/shared/rrthreads2.h"
 
 static ConVar miles_debug("miles_debug", "0", FCVAR_DEVELOPMENTONLY, "Enables debug prints for the Miles Sound System", "1 = print; 0 (zero) = no print");
 static ConVar miles_warnings("miles_warnings", "0", FCVAR_RELEASE, "Enables warning prints for the Miles Sound System", "1 = print; 0 (zero) = no print");
@@ -110,6 +114,312 @@ void CSOM_AddEventToQueue(const char* eventName)
 	}
 };
 
+//-----------------------------------------------------------------------------
+// Purpose: close and reset the CSOM async file instance
+//-----------------------------------------------------------------------------
+static void CSOM_CloseAsyncFile(CSOM_AsyncFile_s* const asyncFile)
+{
+	asyncFile->asyncRequestId = 0;
+	asyncFile->fileName[0] = '\0';
+	FS_CloseAsyncFile(asyncFile->fileHandle);
+	asyncFile->fileHandle = FS_ASYNC_FILE_INVALID;
+	asyncFile->readOffset = 0;
+	asyncFile->fileSize = 0;
+}
+
+//-----------------------------------------------------------------------------
+// Structure for each live file instance
+//-----------------------------------------------------------------------------
+struct CSOM_FileInfo_s
+{
+	char fileName[256];
+	size_t fileSize;
+	int fileHandle;
+};
+
+#define CSOM_MAX_OPENED_FILES 32
+
+static CSOM_FileInfo_s s_milesFileInfos[CSOM_MAX_OPENED_FILES];
+static size_t s_numMilesFilesOpened = 0;
+
+//-----------------------------------------------------------------------------
+// Purpose: finds the file handle for given name, opens it if not found
+//-----------------------------------------------------------------------------
+static int CSOM_MilesAsync_OpenOrFindFile(const char* const fileName, size_t& outFileSize)
+{
+	if (s_numMilesFilesOpened)
+	{
+		// Find the file.
+		CSOM_FileInfo_s* infoIt = s_milesFileInfos;
+		size_t currIdx = 0;
+		bool notFound = false; // If true, will try and open the file.
+
+		while (V_strcmp(fileName, infoIt->fileName))
+		{
+			++currIdx;
+			++infoIt;
+
+			if (currIdx == s_numMilesFilesOpened)
+			{
+				notFound = true;
+				break;
+			}
+		}
+
+		if (!notFound)
+		{
+			outFileSize = infoIt->fileSize;
+			g_pakLoadApi->IncrementAsyncFileRefCount(infoIt->fileHandle);
+
+			return infoIt->fileHandle;
+		}
+	}
+
+	if (s_numMilesFilesOpened == CSOM_MAX_OPENED_FILES)
+		return FS_ASYNC_FILE_INVALID; // Max opened files reached.
+
+	// Open the file.
+	CSOM_FileInfo_s* const info = &s_milesFileInfos[s_numMilesFilesOpened++];
+	V_strncpy(info->fileName, fileName, sizeof(info->fileName));
+
+	info->fileHandle = FS_OpenAsyncFile(fileName, 4, &info->fileSize);
+
+	if (info->fileHandle == FS_ASYNC_FILE_INVALID)
+		Error(eDLL_T::AUDIO, EXIT_FAILURE, "%s( \"%s\" ) failed to open file; try resyncing\n", __FUNCTION__, fileName);
+
+	outFileSize = info->fileSize;
+	g_pakLoadApi->IncrementAsyncFileRefCount(info->fileHandle);
+
+	return info->fileHandle;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: returns the first free file slot index
+//-----------------------------------------------------------------------------
+static inline size_t CSOM_MilesAsync_GetFirstFreeFileSlot()
+{
+	size_t index = 0;
+
+	// Scan the list.
+	while (g_milesGlobals->asyncFiles[index].asyncRequestId)
+		index++;
+
+	return index;
+}
+
+//-----------------------------------------------------------------------------
+// User structure for MilesAsyncRead
+//-----------------------------------------------------------------------------
+struct CSOM_AsyncRead_s
+{
+	int asyncFileHandle;
+	bool shouldCloseFile;
+	bool readFinished;
+};
+
+//-----------------------------------------------------------------------------
+// Purpose: Miles async file read request handler; maps to internal callback of
+//          MilesAsyncFileRead, set through API MilesAsyncSetCallbacks.
+//-----------------------------------------------------------------------------
+static s32 CSOM_MilesAsync_FileRead(MilesAsyncRead* const request)
+{
+	CSOM_AsyncRead_s* const user = (CSOM_AsyncRead_s*)request->Internal;
+	CSOM_AsyncFile_s* asyncFile;
+
+	if (request->RequestId)
+	{
+		asyncFile = &g_milesGlobals->asyncFiles[request->RequestId & CSOM_MAX_ASYNC_FILE_HANDLES_MASK];
+	}
+	else // New request, open the file.
+	{
+		const size_t asyncFileIdx = CSOM_MilesAsync_GetFirstFreeFileSlot();
+		asyncFile = &g_milesGlobals->asyncFiles[asyncFileIdx];
+
+		MilesSubFileInfo_s sfi; char fileNameStack[512];
+		MilesGetSubFileInfo(fileNameStack, request->FileName, &sfi);
+
+		R_UTF8_strncpy(asyncFile->fileName, sfi.filename, sizeof(asyncFile->fileName));
+
+		asyncFile->fileSize = 0;
+		asyncFile->fileHandle = CSOM_MilesAsync_OpenOrFindFile(sfi.filename, asyncFile->fileSize);
+
+		asyncFile->readOffset = 0;
+		asyncFile->readStart = sfi.start;
+
+		if (sfi.size)
+		{
+			const size_t subFileSize = sfi.size + sfi.start;
+
+			if (subFileSize < asyncFile->fileSize)
+				asyncFile->fileSize = subFileSize;
+		}
+
+		// Give the request an unique ID with its slot index packed into it.
+		const u64 asyncRequestId = asyncFileIdx + (++g_milesGlobals->asyncRequestIdGen * CSOM_MAX_ASYNC_FILE_HANDLES);
+
+		asyncFile->asyncRequestId = asyncRequestId;
+		request->RequestId = asyncRequestId;
+	}
+
+	user->shouldCloseFile = (request->Flags & MSSIO_FLAGS_DONT_CLOSE_HANDLE) == 0;
+
+	if ((request->Flags & (MSSIO_FLAGS_QUERY_START_ONLY|MSSIO_FLAGS_QUERY_SIZE_ONLY)) != 0)
+		request->Start = asyncFile->fileSize - asyncFile->readStart;
+
+	size_t readCount = request->Count;
+
+	if ((request->Flags & MSSIO_FLAGS_QUERY_SIZE_ONLY) != 0 || readCount == 0)
+	{
+		user->asyncFileHandle = FS_ASYNC_FILE_INVALID;
+		request->Status = MSSIO_STATUS_COMPLETE;
+
+		if (user->shouldCloseFile)
+			CSOM_CloseAsyncFile(asyncFile);
+
+		return 1;
+	}
+
+	size_t readOffset = 0;
+
+	if ((request->Flags & MSSIO_FLAGS_DONT_USE_OFFSET) == 0)
+	{
+		readOffset = request->Offset;
+		asyncFile->readOffset = readOffset;
+	}
+
+	if (readCount < 0)
+	{
+		readCount = asyncFile->fileSize - asyncFile->readStart - readOffset;
+		request->Count = readCount;
+	}
+
+	size_t numBytesLeft = asyncFile->fileSize - asyncFile->readStart - readOffset;
+
+	if (readCount < numBytesLeft)
+		numBytesLeft = readCount;
+
+	request->Count = numBytesLeft;
+
+	if (!request->Buffer)
+	{
+		// Allocate a read buffer.
+		const size_t readBufSize = numBytesLeft + request->ReadAmt;
+		void* const readBuffer = v_MilesAllocEx(readBufSize, 0, g_milesGlobals->driver, request->LastAllocSrcFileName, request->LastAllocSrcFileLine);
+
+		if (!readBuffer)
+		{
+			Error(eDLL_T::AUDIO, EXIT_FAILURE, "Miles async failed malloc for '%s' size %zu\n", asyncFile->fileName, readBufSize);
+
+			user->asyncFileHandle = FS_ASYNC_FILE_INVALID;
+			request->Status = MSSIO_STATUS_ERROR_MEMORY_ALLOC_FAIL;
+
+			if (user->shouldCloseFile)
+				CSOM_CloseAsyncFile(asyncFile);
+
+			return 0;
+		}
+
+		request->Buffer = readBuffer;
+	}
+
+	if (request->Count)
+	{
+		// Read data into the buffer.
+		user->readFinished = false;
+		user->asyncFileHandle = g_pakLoadApi->ReadAsyncFile(asyncFile->fileHandle, asyncFile->readStart + asyncFile->readOffset, request->Count, request->Buffer, 1);
+
+		if (user->asyncFileHandle == FS_ASYNC_FILE_INVALID)
+		{
+			Error(eDLL_T::AUDIO, EXIT_FAILURE, "Miles async failed read for '%s' offset %zu count %zu\n", asyncFile->fileName, request->Offset, request->Count);
+			request->Status = MSSIO_STATUS_ERROR_FAILED_OPEN;
+
+			if (user->shouldCloseFile)
+				CSOM_CloseAsyncFile(asyncFile);
+
+			return 0;
+		}
+
+		request->Status = MSSIO_STATUS_COMPLETE_NOP;
+	}
+	else
+	{
+		request->Status = MSSIO_STATUS_COMPLETE_NOP;
+		user->readFinished = true;
+		user->asyncFileHandle = FS_ASYNC_REQ_INVALID;
+	}
+
+	return 1;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Miles async file status request handler; maps to internal callback
+//          of MilesAsyncFileStatus, set through API MilesAsyncSetCallbacks.
+//-----------------------------------------------------------------------------
+static s32 CSOM_MilesAsync_FileStatus(MilesAsyncRead* const request, const u32 i_MS)
+{
+	CSOM_AsyncRead_s* const user = (CSOM_AsyncRead_s*)request->Internal;
+
+	if (user->asyncFileHandle == FS_ASYNC_FILE_INVALID)
+		return request->Status;
+
+	AsyncHandleStatus_s::Status_e currentStatus;
+
+	if (user->readFinished)
+	{
+		currentStatus = AsyncHandleStatus_s::Status_e::FS_ASYNC_READY;
+	}
+	else
+	{
+		if (i_MS)
+			g_pakLoadApi->WaitForAsyncRequest(user->asyncFileHandle);
+
+		currentStatus = g_pakLoadApi->CheckAsyncRequest(user->asyncFileHandle, nullptr, nullptr);
+
+		if (currentStatus == AsyncHandleStatus_s::Status_e::FS_ASYNC_PENDING)
+			return 0;
+	}
+
+	user->asyncFileHandle = FS_ASYNC_FILE_INVALID;
+	CSOM_AsyncFile_s* const asyncFile = &g_milesGlobals->asyncFiles[request->RequestId & CSOM_MAX_ASYNC_FILE_HANDLES_MASK];
+
+	if (user->shouldCloseFile)
+		CSOM_CloseAsyncFile(asyncFile);
+
+	if (currentStatus == AsyncHandleStatus_s::Status_e::FS_ASYNC_READY)
+	{
+		request->LastCount = request->Count;
+		asyncFile->readOffset += request->Count;
+		request->Status = MSSIO_STATUS_COMPLETE;
+	}
+	else // Failure or canceled.
+	{
+		request->LastCount = 0;
+
+		if (currentStatus == AsyncHandleStatus_s::Status_e::FS_ASYNC_CANCELLED)
+			request->Status = MSSIO_STATUS_ERROR_CANCELLED;
+		else
+			request->Status = MSSIO_STATUS_ERROR_FAILED_READ;
+	}
+
+	return request->Status;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Miles async file cancel request handler; maps to internal callback
+//          of MilesAsyncFileCancel, set through API MilesAsyncSetCallbacks.
+//-----------------------------------------------------------------------------
+static s32 CSOM_MilesAsync_FileCancel(MilesAsyncRead* const request)
+{
+	CSOM_AsyncRead_s* const user = (CSOM_AsyncRead_s*)request->Internal;
+
+	if (user->asyncFileHandle == FS_ASYNC_FILE_INVALID)
+		return 1; // Nothing to cancel.
+
+	if (!user->readFinished)
+		g_pakLoadApi->CancelAsyncRequest(user->asyncFileHandle);
+
+	return CSOM_MilesAsync_FileStatus(request, RR_WAIT_INFINITE);
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 void MilesCore::Detour(const bool bAttach) const
@@ -118,5 +428,8 @@ void MilesCore::Detour(const bool bAttach) const
 	DetourSetup(&v_Miles_Initialize, &Miles_Initialize, bAttach);
 	DetourSetup(&v_MilesQueueEventRun, &MilesQueueEventRun, bAttach);
 	//DetourSetup(&v_MilesBankPatch, &MilesBankPatch, bAttach);
+	DetourSetup(&v_CSOM_MilesAsync_FileRead, &CSOM_MilesAsync_FileRead, bAttach);
+	DetourSetup(&v_CSOM_MilesAsync_FileStatus, &CSOM_MilesAsync_FileStatus, bAttach);
+	DetourSetup(&v_CSOM_MilesAsync_FileCancel, &CSOM_MilesAsync_FileCancel, bAttach);
 	DetourSetup(&v_CSOM_AddEventToQueue, &CSOM_AddEventToQueue, bAttach);
 }

src/codecs/miles/miles_impl.h
@@ -1,14 +1,34 @@
 #pragma once
 #include "miles_types.h"
+#include "miles/include/mss.h"
+
+#define CSOM_MAX_ASYNC_FILE_HANDLES 128
+#define CSOM_MAX_ASYNC_FILE_HANDLES_MASK (CSOM_MAX_ASYNC_FILE_HANDLES-1)
 
 /* ==== WASAPI THREAD SERVICE =========================================================================================================================================== */
 inline void(*v_AIL_LogFunc)(int64_t nLogLevel, const char* pszMessage);
 inline bool(*v_Miles_Initialize)();
+inline void*(*v_MilesAllocEx)(const size_t size, const u8 flags, void* const driver, const char* const allocSrcFileName, const int allocSrcFileLine);
 inline void(*v_MilesQueueEventRun)(Miles::Queue*, const char*);
 inline void(*v_MilesBankPatch)(Miles::Bank*, char*, char*);
 inline unsigned int (*v_MilesSampleSetSourceRaw)(__int64 a1, __int64 a2, unsigned int a3, int a4, unsigned __int16 a5, bool a6);
 inline unsigned int (*v_MilesEventGetDetails)(__int64 a1, __int64 a2, __int64 a3, __int64 a4, __int64 a5, __int64 a6, void* const releaseList);
+
+inline s32(*v_CSOM_MilesAsync_FileRead)(MilesAsyncRead* const request);
+inline s32(*v_CSOM_MilesAsync_FileStatus)(MilesAsyncRead* const request, const u32 i_MS);
+inline s32(*v_CSOM_MilesAsync_FileCancel)(MilesAsyncRead* const request);
+
 inline void(*v_CSOM_AddEventToQueue)(const char* eventName);
+
+struct CSOM_AsyncFile_s
+{
+	u64 asyncRequestId;
+	char fileName[64];
+	int fileHandle;
+	size_t readOffset;
+	size_t readStart;
+	size_t fileSize;
+};
 
 struct MilesBankList_t
 {
@@ -37,7 +57,10 @@ struct MilesGlobalState_t
 	MilesBankList_t bankList;
 	char gap52c[4];
 	void* loadedBanks[16];
-	char gap5b0[290448];
+	char gap5b0[273016];
+	CSOM_AsyncFile_s asyncFiles[CSOM_MAX_ASYNC_FILE_HANDLES];
+	size_t asyncRequestIdGen;
+	char gap46430[4110];
 	HANDLE milesInitializedEvent;
 	HANDLE milesThread;
 	char gap47450[272];
@@ -54,16 +77,23 @@ class MilesCore : public IDetour
 	{
 		LogFunAdr("AIL_LogFunc", v_AIL_LogFunc);
 		LogFunAdr("Miles_Initialize", v_Miles_Initialize);
+		LogFunAdr("MilesAllocEx", v_MilesAllocEx);
 		LogFunAdr("MilesQueueEventRun", v_MilesQueueEventRun);
 		LogFunAdr("MilesBankPatch", v_MilesBankPatch);
 		LogFunAdr("MilesSampleSetSourceRaw", v_MilesSampleSetSourceRaw);
 		LogFunAdr("MilesEventGetDetails", v_MilesEventGetDetails);
+		LogFunAdr("CSOM_MilesAsync_FileRead", v_CSOM_MilesAsync_FileRead);
+		LogFunAdr("CSOM_MilesAsync_FileStatus", v_CSOM_MilesAsync_FileStatus);
+		LogFunAdr("CSOM_MilesAsync_FileCancel", v_CSOM_MilesAsync_FileCancel);
 		LogFunAdr("CSOM_AddEventToQueue", v_CSOM_AddEventToQueue);
 		LogVarAdr("g_milesGlobals", g_milesGlobals);
 	}
 	virtual void GetFun(void) const
 	{
 		Module_FindPattern(g_GameDll, "40 53 48 83 EC 20 48 8B DA 48 8D 15 ?? ?? ?? ??").GetPtr(v_AIL_LogFunc);
+		Module_FindPattern(g_GameDll, "4C 8B DC 53 55 56 57 48 81 EC").GetPtr(v_CSOM_MilesAsync_FileRead);
+		Module_FindPattern(g_GameDll, "48 89 5C 24 ?? 57 48 83 EC ?? 8B B9 ?? ?? ?? ?? 48 8B D9 83 FF").GetPtr(v_CSOM_MilesAsync_FileStatus);
+		Module_FindPattern(g_GameDll, "40 53 48 83 EC ?? 8B 81 ?? ?? ?? ?? 48 8B D9 83 F8 ?? 75 ?? B8").GetPtr(v_CSOM_MilesAsync_FileCancel);
 		Module_FindPattern(g_GameDll, "0F B6 11 4C 8B C1").GetPtr(v_CSOM_AddEventToQueue);
 		
 		CMemory milesInitializeFunc = Module_FindPattern(g_GameDll, "40 53 56 57 41 54 41 55 41 56 41 57 48 81 EC ?? ?? ?? ?? 80 3D ?? ?? ?? ?? ??");
@@ -71,6 +101,7 @@ class MilesCore : public IDetour
 
 		g_milesGlobals = milesInitializeFunc.FindPatternSelf("48 8D", CMemory::Direction::DOWN, 0x50).ResolveRelativeAddressSelf(0x3, 0x7).RCast<MilesGlobalState_t*>();
 
+		g_RadAudioSystemDll.GetExportedSymbol("MilesAllocEx").GetPtr(v_MilesAllocEx);
 		g_RadAudioSystemDll.GetExportedSymbol("MilesQueueEventRun").GetPtr(v_MilesQueueEventRun);
 		g_RadAudioSystemDll.GetExportedSymbol("MilesBankPatch").GetPtr(v_MilesBankPatch);
 		g_RadAudioSystemDll.GetExportedSymbol("MilesSampleSetSourceRaw").GetPtr(v_MilesSampleSetSourceRaw);

commit 91dac0c0d8f2e5b836d7890e1760ff2466d78f8d

Miles: fix closed caption regression
Regression was introduced in commit 17b37ff7d2e49810c532fdf24f493372b37cc32d when the Miles Sound System was upgraded to version 10.0.62.0. The API MilesEventInfoQueueEnum now returns a slightly different struct for enum 4, where 2 members have been swapped in memory locations, one of them is the duration of the event which the closed caption system needs to determine how long to show the text. Engine now reflects the new changes and closed captions now work as intended.

src/codecs/miles/miles_impl.cpp
@@ -432,4 +432,18 @@ void MilesCore::Detour(const bool bAttach) const
 	DetourSetup(&v_CSOM_MilesAsync_FileStatus, &CSOM_MilesAsync_FileStatus, bAttach);
 	DetourSetup(&v_CSOM_MilesAsync_FileCancel, &CSOM_MilesAsync_FileCancel, bAttach);
 	DetourSetup(&v_CSOM_AddEventToQueue, &CSOM_AddEventToQueue, bAttach);
+
+	if (bAttach)
+	{
+		CMemory mem(v_CSOM_RunFrame);
+
+		// Between Miles version 10.0.48 and 10.0.50, they swapped locations of
+		// 2 members in a struct returned by MilesEventInfoQueueEnum on type 4.
+		// This change breaks closed captions (sub-titles). The fix is to apply
+		// the swap in the assembly code as well so the engine retrieves the
+		// values correctly from the new locations again. The structure layout
+		// on all other enums are still identical and do not need to be fixes.
+		mem.Offset(0x762).Patch({ 0x4 });
+		mem.Offset(0x78B).Patch({ 0xC });
+	}
 }

src/codecs/miles/miles_impl.h
@@ -14,6 +14,8 @@ inline unsigned int (*v_MilesEventGetDetails)(__int64 a1, __int64 a2, __int64 a3
 
 inline bool(*v_CSOM_Initialize)();
 inline void(*v_CSOM_LogFunc)(int64_t nLogLevel, const char* pszMessage);
+inline void(*v_CSOM_RunFrame)(char a1, char a2, float a3, float a4);
+
 inline s32(*v_CSOM_MilesAsync_FileRead)(MilesAsyncRead* const request);
 inline s32(*v_CSOM_MilesAsync_FileStatus)(MilesAsyncRead* const request, const u32 i_MS);
 inline s32(*v_CSOM_MilesAsync_FileCancel)(MilesAsyncRead* const request);
@@ -82,6 +84,7 @@ class MilesCore : public IDetour
 		LogFunAdr("MilesEventGetDetails", v_MilesEventGetDetails);
 		LogFunAdr("CSOM_Initialize", v_CSOM_Initialize);
 		LogFunAdr("CSOM_LogFunc", v_CSOM_LogFunc);
+		LogFunAdr("CSOM_RunFrame", v_CSOM_RunFrame);
 		LogFunAdr("CSOM_MilesAsync_FileRead", v_CSOM_MilesAsync_FileRead);
 		LogFunAdr("CSOM_MilesAsync_FileStatus", v_CSOM_MilesAsync_FileStatus);
 		LogFunAdr("CSOM_MilesAsync_FileCancel", v_CSOM_MilesAsync_FileCancel);
@@ -92,6 +95,7 @@ class MilesCore : public IDetour
 	{
 		Module_FindPattern(g_GameDll, "40 53 56 57 41 54 41 55 41 56 41 57 48 81 EC ?? ?? ?? ?? 80 3D ?? ?? ?? ?? ??").GetPtr(v_CSOM_Initialize);
 		Module_FindPattern(g_GameDll, "40 53 48 83 EC 20 48 8B DA 48 8D 15 ?? ?? ?? ??").GetPtr(v_CSOM_LogFunc);
+		Module_FindPattern(g_GameDll, "48 8B C4 F3 0F 11 50 ?? 88 50").GetPtr(v_CSOM_RunFrame);
 		Module_FindPattern(g_GameDll, "4C 8B DC 53 55 56 57 48 81 EC").GetPtr(v_CSOM_MilesAsync_FileRead);
 		Module_FindPattern(g_GameDll, "48 89 5C 24 ?? 57 48 83 EC ?? 8B B9 ?? ?? ?? ?? 48 8B D9 83 FF").GetPtr(v_CSOM_MilesAsync_FileStatus);
 		Module_FindPattern(g_GameDll, "40 53 48 83 EC ?? 8B 81 ?? ?? ?? ?? 48 8B D9 83 F8 ?? 75 ?? B8").GetPtr(v_CSOM_MilesAsync_FileCancel);