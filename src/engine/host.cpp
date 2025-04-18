dilirity/r5sdk

commit c5a7c0c14c49469297d5d9e698d4e1d1de31281f

Engine: imporve function parameter name
This is delta time.

src/engine/host.cpp
@@ -60,7 +60,7 @@ _Host_RunFrame
 Runs all active servers
 ==================
 */
-void _Host_RunFrame(void* unused, float time)
+void _Host_RunFrame(void* unused, const float deltaTime)
 {
 	for (IFrameTask* const& task : g_TaskQueueList)
 	{
@@ -80,7 +80,7 @@ void _Host_RunFrame(void* unused, float time)
 	DebugOverlay_HandleDecayed();
 #endif // DEDICATED
 
-	v_Host_RunFrame(unused, time);
+	v_Host_RunFrame(unused, deltaTime);
 }
 
 void Host_Error(const char* const error, ...)

commit 6ed287fcb43fd545b33ae001dc5ac3d2a78246ca

Engine: fix dedicated server compile error
Introduced in commit c5a7c0c14c49469297d5d9e698d4e1d1de31281f, rename was not applied on dedicated server code.

src/engine/host.cpp
@@ -73,7 +73,7 @@ void _Host_RunFrame(void* unused, const float deltaTime)
 		}), g_TaskQueueList.end());
 
 #ifndef DEDICATED
-	g_TextOverlay.ShouldDraw(time);
+	g_TextOverlay.ShouldDraw(deltaTime);
 #endif // !DEDICATED
 
 #ifdef DEDICATED