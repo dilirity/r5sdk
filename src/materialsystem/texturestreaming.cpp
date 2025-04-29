046db25cec36a382c5d0a30a5f4faa7c9ec5a7de
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2025-04-30 01:50:47 +0200

    MaterialSystem: only set new cvar value if it changed
    Only set it if we changed it, this check is way cheaper than calling SetValue().

--- src/materialsystem/texturestreaming.cpp
+++ src/materialsystem/texturestreaming.cpp
@@ -27,7 +27,9 @@ static void StreamDB_Init(const char* const pszLevelName)
 	// If the requested STBSP file doesn't exist, then enable the GPU driven
 	// texture streaming system.
 	const bool gpuDriven = s_textureStreamMgr->fileHandle == FS_ASYNC_FILE_INVALID;
-	gpu_driven_tex_stream->SetValue(gpuDriven);
+
+	if (gpu_driven_tex_stream->GetBool() != gpuDriven)
+		gpu_driven_tex_stream->SetValue(gpuDriven);
 
 	if (!gpuDriven)
 		Msg(eDLL_T::MS, "StreamDB_Init: Loaded STBSP file '%s.stbsp'\n", targetStreamDB);
