e4496dbdba348939ffc1b5086139a702d5425ccf
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2025-05-02 21:46:41 +0200

    Tier1: decouple SDK specific code
    Avoid having the linker link this code inside our plugin libraries.

--- src/tier1/cvar.cpp
+++ src/tier1/cvar.cpp
@@ -117,7 +117,7 @@ static void ConVar_AppendFlags(const ConCommandBase* const var, char* buf, size_
 //-----------------------------------------------------------------------------
 // Purpose: 
 //-----------------------------------------------------------------------------
-static void ConVar_PrintDescription(const ConCommandBase* const pVar)
+void ConVar_PrintDescription(const ConCommandBase* const pVar)
 {
 	Assert(pVar);
 
@@ -676,69 +676,5 @@ int CCvarUtilities::CvarFindFlagsCompletionCallback(const char* partial,
 	return values;
 }
 
-/*
-=====================
-CON_Help_f
-
-  Shows the colors and
-  description of each
-  context.
-=====================
-*/
-static void CON_Help_f()
-{
-	Msg(eDLL_T::COMMON, "Contexts:\n");
-
-	Msg(eDLL_T::SCRIPT_SERVER, " = Server DLL (Script)\n");
-	Msg(eDLL_T::SCRIPT_CLIENT, " = Client DLL (Script)\n");
-	Msg(eDLL_T::SCRIPT_UI, " = UI DLL (Script)\n");
-
-	Msg(eDLL_T::SERVER, " = Server DLL (Code)\n");
-	Msg(eDLL_T::CLIENT, " = Client DLL (Code)\n");
-	Msg(eDLL_T::UI, " = UI DLL (Code)\n");
-
-	Msg(eDLL_T::ENGINE, " = Engine DLL (Code)\n");
-	Msg(eDLL_T::FS, " = FileSystem (Code)\n");
-	Msg(eDLL_T::RTECH, " = PakLoad API (Code)\n");
-	Msg(eDLL_T::MS, " = MaterialSystem (Code)\n");
-
-	Msg(eDLL_T::AUDIO, " = Audio DLL (Code)\n");
-	Msg(eDLL_T::VIDEO, " = Video DLL (Code)\n");
-	Msg(eDLL_T::NETCON, " = NetConsole (Code)\n");
-}
-
-static ConCommand con_help("con_help", CON_Help_f, "Shows the colors and description of each context", FCVAR_RELEASE);
-
 ///////////////////////////////////////////////////////////////////////////////
 CCvar* g_pCVar = nullptr;
-
-
-static bool CVar_Connect(CCvar* thisptr, CreateInterfaceFn factory)
-{
-	CCvar__Connect(thisptr, factory);
-
-	ConVar_InitShipped();
-	ConVar_PurgeShipped();
-	ConCommand_InitShipped();
-	ConCommand_PurgeShipped();
-
-	ConVar_Register();
-
-	// CCvar::Connect() always returns true in the implementation of the engine
-	return true;
-}
-
-static void CVar_Disconnect(CCvar* thisptr)
-{
-	ConVar_Unregister();
-	CCvar__Disconnect(thisptr);
-}
-
-///////////////////////////////////////////////////////////////////////////////
-void VCVar::Detour(const bool bAttach) const
-{
-	DetourSetup(&CCvar__Connect, &CVar_Connect, bAttach);
-	DetourSetup(&CCvar__Disconnect, &CVar_Disconnect, bAttach);
-
-	DetourSetup(&v_ConVar_PrintDescription, &ConVar_PrintDescription, bAttach);
-}
